69c1635b4243c9a14bbad9c2de930c4fd3146c96
IsaacChen <tingyi364@gmail.com>
2017-08-13 23:04:48 +0800

    vince: Blacklist FP event
     * After screen unlocked,blacklist FP event to avoid some misbehavior.
    
    Signed-off-by: Isaac Chen <isaacchen@isaacchen.cn>

--- product/keylayout.mk
+++ product/keylayout.mk
@@ -4,3 +4,8 @@ PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/keylayout/synaptics_dsx_v21.kl:system/usr/keylayout/synaptics_dsx_v21.kl \
     $(DEVICE_PATH)/keylayout/uinput-fpc.kl:system/usr/keylayout/uinput-fpc.kl \
     $(DEVICE_PATH)/keylayout/uinput-goodix.kl:system/usr/keylayout/uinput-goodix.kl
+
+# IDC
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/idc/uinput-fpc.idc:system/usr/idc/uinput-fpc.idc \
+    $(DEVICE_PATH)/idc/uinput-goodix.idc:system/usr/idc/uinput-goodix.idc
