e619463525b55b32fd8838ad3d61368796587d74
Jesse Hall <jessehall@google.com>
2017-08-29 15:46:29 -0700

    vince: Declare Vulkan compute level 0 system feature
    Bug: 38396207 (VK_KHR_variable_pointers)
    Bug: 64982680 (maxPerStageDescriptorStorageBuffers >= 16)
    Test: CtsGraphicsTestCases:.VulkanFeaturesTest (ag/2810565)
    Change-Id: I61cde61f53f0e9a1c657e6bd6658a4393360b0a6
    Signed-off-by: Isaac Chen <isaacchen@isaacchen.cn>

--- product/qcom-display.mk
+++ product/qcom-display.mk
@@ -21,5 +21,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Vulkan
 PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.vulkan.compute-0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.compute.xml \
     frameworks/native/data/etc/android.hardware.vulkan.level-0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.level-0.xml \
     frameworks/native/data/etc/android.hardware.vulkan.version-1_0_3.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.version-1_0_3.xml
