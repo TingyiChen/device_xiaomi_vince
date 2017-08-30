8a7e9b6cfbe18ad974690dc83698c41b4c765e60
Vitaliy Tomin <highwaystar.ru@gmail.com>
2017-08-30 21:04:31 +0800

    vince: Add seccomp policy
    Change-Id: I2dcc7b256044dcf8db997359b3c4f785daae4cab

--- product/media.mk
+++ product/media.mk
@@ -9,6 +9,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml
 
+# Seccomp policy
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/seccomp_policy/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
+
 # Properties
 PRODUCT_PROPERTY_OVERRIDES += \
     mm.enable.smoothstreaming=true \
