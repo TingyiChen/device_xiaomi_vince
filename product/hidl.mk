804840737019b0057788d6d11408de65faa0431e
Joey <joey@lineageos.org>
2018-02-07 19:04:19 +0530

    vince: Build textclassifier
    Change-Id: I50d8173d24c4cab773824107b98929dbbc469783

--- product/hidl.mk
+++ product/hidl.mk
@@ -104,6 +104,10 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
     android.hardware.thermal@1.0-service
 
+# TextClassifier smart selection model files
+PRODUCT_PACKAGES += \
+    textclassifier.smartselection.bundle1
+
 # USB HAL
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
