9108e82bc29d4abb07ed7c2abb6483d18adfc0cf
HeeRyun, Jin <jins4218@gmail.com>
2018-02-09 11:36:48 +0900

    vince: Update rootdir
    Signed-off-by: HeeRyun, Jin <jins4218@gmail.com>
    Signed-off-by: Isaac Chen <isaacchen@isaacchen.cn>

--- product/init.mk
+++ product/init.mk
@@ -1,35 +1,13 @@
 # Init scripts
 PRODUCT_PACKAGES += \
-    init.qcom.class_core.sh \
-    init.qcom.ssr.sh \
-    init.qcom.sensors.sh \
-    init.qcom.early_boot.sh \
-    init.qcom.syspart_fixup.sh \
     fstab.qcom \
+    init.qcom.sh \
     init.qcom.rc \
     init.qcom.usb.rc \
-    init.qcom.power.rc \
     init.qcom.usb.sh \
-    init.qcom.factory.rc \
     init.target.rc \
     ueventd.qcom.rc
 
 PRODUCT_PACKAGES += \
-    init.qcom.wifi.sh \
-    init.ath4k.bt.sh \
-    init.qcom.uicc.sh \
-    init.qcom.efs.sync.sh \
-    init.qti.ims.sh \
-    hcidump.sh \
     init.qcom.post_boot.sh \
-    init.qcom.fm.sh \
-    init.qcom.sdio.sh \
-    init.crda.sh \
-    qca6234-service.sh \
-    init.qcom.coex.sh \
-    init.qcom.audio.sh \
-    hsic.control.bt.sh \
     init.qcom.bt.sh
-
-PRODUCT_PACKAGES += \
-    init.qcom.sh
