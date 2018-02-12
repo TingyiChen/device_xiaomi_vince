06583add080a3d91e443dbcd9d798986f6d7b590
Isaac Chen <isaacchen@isaacchen.cn>
2018-02-12 09:45:18 +0800

    vince: rootdir: Cleanup
    Signed-off-by: Isaac Chen <isaacchen@isaacchen.cn>

--- product/init.mk
+++ product/init.mk
@@ -1,6 +1,7 @@
 # Init scripts
 PRODUCT_PACKAGES += \
     fstab.qcom \
+    init.qcom.sh \
     init.qcom.rc \
     init.qcom.usb.rc \
     init.qcom.usb.sh \
@@ -8,21 +9,5 @@ PRODUCT_PACKAGES += \
     ueventd.qcom.rc
 
 PRODUCT_PACKAGES += \
-    init.qcom.wifi.sh \
-    init.ath4k.bt.sh \
-    init.qcom.uicc.sh \
-    init.qcom.efs.sync.sh \
-    init.qti.ims.sh \
-    hcidump.sh \
     init.qcom.post_boot.sh \
-    init.qcom.fm.sh \
-    init.qcom.sdio.sh \
-    init.crda.sh \
-    qca6234-service.sh \
-    init.qcom.coex.sh \
-    init.qcom.audio.sh \
-    hsic.control.bt.sh \
     init.qcom.bt.sh
-
-PRODUCT_PACKAGES += \
-    init.qcom.sh
