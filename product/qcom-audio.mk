82e18255ebe7fbc41e4397679154082e7d6efeb9
Isaac Chen <isaacchen@isaacchen.cn>
2018-02-15 14:56:42 +0100

    vince: Remove BSP audio effect config
    Signed-off-by: Isaac Chen <isaacchen@isaacchen.cn>

--- product/qcom-audio.mk
+++ product/qcom-audio.mk
@@ -17,7 +17,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
     $(LOCAL_PATH)/audio/audio_output_policy.conf:system/vendor/etc/audio_output_policy.conf \
-    $(LOCAL_PATH)/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf \
     $(LOCAL_PATH)/audio/mixer_paths_vince.xml:system/etc/mixer_paths_vince.xml \
     $(LOCAL_PATH)/audio/sound_trigger_mixer_paths.xml:system/etc/sound_trigger_mixer_paths.xml \
     $(LOCAL_PATH)/audio/sound_trigger_platform_info.xml:system/etc/sound_trigger_platform_info.xml \
