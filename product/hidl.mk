b4119b91f33f96a8851bc1ee5935cc3632b13419
Bruno Martins <bgcngm@gmail.com>
2018-03-18 21:20:20 +0000

    vince: Remove non-existing HAL services
    Change-Id: I26da013dfa359ec8f184a20bc881558be0144b4a

--- product/hidl.mk
+++ product/hidl.mk
@@ -3,9 +3,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@2.0-impl \
-    android.hardware.audio.effect@2.0-service \
-    android.hardware.soundtrigger@2.0-impl \
-    android.hardware.soundtrigger@2.0-service
+    android.hardware.soundtrigger@2.0-impl
 
 # Bluetooth
 PRODUCT_PACKAGES += \
