daccbf4f7202b5e43d34ef7b1f45b50146ed79a1
Isaac Chen <isaacchen@isaacchen.cn>
2018-04-04 14:23:11 +0800

    vince: Apply lineage rebrand
    Signed-off-by: Isaac Chen <isaacchen@isaacchen.cn>

--- board/hardware.mk
+++ board/hardware.mk
@@ -1,4 +1,3 @@
 # CM Hardware
-BOARD_USES_CYANOGEN_HARDWARE := true
 BOARD_HARDWARE_CLASS += \
-    hardware/cyanogen/cmhw
+    hardware/lineage/lineagehw
--- lineage.mk
+++ lineage.mk
@@ -20,7 +20,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from vince device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
