3515713adae9faa7eca24f6d4ebd66e1624f694d
Isaac Chen <isaacchen@isaacchen.cn>
2018-04-15 10:52:42 +0800

    vince: Add Jack keylayout
    Signed-off-by: Isaac Chen <isaacchen@isaacchen.cn>

--- product/keylayout.mk
+++ product/keylayout.mk
@@ -1,6 +1,7 @@
 # Keylayouts
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
+    $(DEVICE_PATH)/keylayout/msm8953-snd-card-mtp_Button_Jack.kl:system/usr/keylayout/msm8953-snd-card-mtp_Button_Jack.kl \
     $(DEVICE_PATH)/keylayout/synaptics_dsx_v21.kl:system/usr/keylayout/synaptics_dsx_v21.kl \
     $(DEVICE_PATH)/keylayout/uinput-fpc.kl:system/usr/keylayout/uinput-fpc.kl \
     $(DEVICE_PATH)/keylayout/uinput-goodix.kl:system/usr/keylayout/uinput-goodix.kl
