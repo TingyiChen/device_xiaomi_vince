61b00ab7600879f7e5d24cf6ea217d14dea460f0
Michael Bestas <mkbestas@lineageos.org>
2018-05-27 18:04:11 +0300

    vince: Build VR HAL
    Change-Id: I641c5f2bcbf31ff5a1dd26c9feb6209f7a5a633c

--- product/hidl.mk
+++ product/hidl.mk
@@ -112,6 +112,11 @@ PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl \
     android.hardware.vibrator@1.0-service
 
+# Virtual Reality
+PRODUCT_PACKAGES += \
+    android.hardware.vr@1.0-impl \
+    android.hardware.vr@1.0-service
+
 # Wifi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service
---
+++ product/vr.mk
@@ -0,0 +1,7 @@
+# Virtual Reality
+PRODUCT_PACKAGES += \
+    vr.msm8953
+
+# Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.vr.high_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vr.high_performance.xml
