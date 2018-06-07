f3b3669920250c488faf6f68b1e1d9c7a0ef4fa3
Isaac Chen <isaacchen@isaacchen.cn>
2018-06-07 22:34:19 +0800

    vince: Cleanup HIDL services
     * Thermal HIDL need thermal.msm8953 to work
    
    Signed-off-by: Isaac Chen <isaacchen@isaacchen.cn>

--- product/hidl.mk
+++ product/hidl.mk
@@ -107,10 +107,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     textclassifier.smartselection.bundle1
 
-# USB HAL
-PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service
-
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl \
