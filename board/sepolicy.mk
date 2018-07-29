5836e1763b9bf0d41545440e070a92938cae5bbc
Isaac Chen <isaacchen@isaacchen.cn>
2018-07-29 05:01:23 +0200

    [TMP]vince: Sync from 8.7.26
    Signed-off-by: Isaac Chen <isaacchen@isaacchen.cn>

--- board/sepolicy.mk
+++ board/sepolicy.mk
@@ -1,6 +1,6 @@
 # SELinux
 BOARD_SEPOLICY_DIRS += \
-    $(DEVICE_PATH)/sepolicy
+    device/xiaomi/vince/sepolicy
 
 include device/qcom/sepolicy/sepolicy.mk
 include device/qcom/sepolicy/legacy-sepolicy.mk
