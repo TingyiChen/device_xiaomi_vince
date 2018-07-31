0a3feb0aa20f950bc13bfed9ef5ae4fe3aca57ae
Isaac Chen <isaacchen@isaacchen.cn>
2018-07-31 05:03:01 +0200

    vince: AEX-ify
    Signed-off-by: Isaac Chen <isaacchen@isaacchen.cn>

---
+++ AndroidProducts.mk
@@ -0,0 +1,1 @@
+PRODUCT_MAKEFILES := $(LOCAL_DIR)/aosp_vince.mk
--- aosp_vince.mk
+++ aosp_vince.mk
@@ -20,15 +20,17 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/common.mk)
 
 # Inherit from vince device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
+TARGET_BOOT_ANIMATION_RES := 1080
+
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := vince
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := lineage_vince
+PRODUCT_NAME := aosp_vince
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
